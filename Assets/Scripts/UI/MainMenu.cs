f06cc0f25699345f3ef99bbe56ac89f51d16b3fb
FBlood007 <jambhle.omkar300@gmail.com>
2023-10-12 18:25:36 +0530

    add main screen buttons

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject settings;
+    public GameObject highScore;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
