55fbd1df67db4e69d536609c89781ce40be19c28
FBlood007 <jambhle.omkar300@gmail.com>
2023-10-16 18:28:07 +0530

    add player jump with double jump

---
+++ Assets/Parallax.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public float depth = 1;
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velocityReal = player.velocity.x / depth * 0.025f;
+        Vector2 pos = transform.position;
+        pos.x -= velocityReal * Time.fixedDeltaTime;
+
+
+        if (pos.x <= -25)
+            pos.x = 45; 
+
+        transform.position = pos;
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,11 @@ public class Player : MonoBehaviour
     public float acceleration = 10f;
     public float maxAcceleration = 10f;
     public float maxXVelocity = 90f;
-    public float groundHeight = 5f;
+    public float groundHeight = 10f;
     public bool isGrounded = false;
     public float jumpVelocity = 5f;
     public float distance = 0f;
-
+    public float maxMaxxHoldJumpTime = 0.4f;
     public bool isTouched = false;
     public float maxTouchedJumpTime = 0.2f;
     public float touchJumpTimer = 0.0f;
@@ -34,9 +34,9 @@ public class Player : MonoBehaviour
         Vector2 pos = transform.position;
         float groundDistance = Mathf.Abs(pos.y - groundHeight);
 
-        if (isGrounded || groundDistance <= jumpGroundThreshold)
+        if (isGrounded || groundDistance <= jumpGroundThreshold )
         {
-            if(Input.touchCount > 0 && Input.GetTouch(0).phase != TouchPhase.Began) {
+            if(Input.touchCount > 0 && Input.GetTouch(0).phase != TouchPhase.Began && transform.position.y >= -5f) {
                 isGrounded = false;
                 velocity.y = jumpVelocity;   
                 touchJumpTimer = 0;
@@ -73,11 +73,26 @@ public class Player : MonoBehaviour
             {
                 velocity.y += gravity * Time.fixedDeltaTime;
             }
-            if(pos.y <= groundHeight)
+
+            Vector2 rayOrigin = new Vector2(pos.x + 0.7f, pos.y);
+            Vector2 rayDirection = Vector2.up;
+            float rayDistance = velocity.y * Time.fixedDeltaTime;
+            RaycastHit2D hit2d = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+            if (hit2d.collider != null)
             {
-                pos.y = groundHeight;
-                isGrounded=true;
+                 Cloud cloud = hit2d.collider.GetComponent<Cloud>();    
+                if(cloud != null)
+                {
+                    pos.y = groundHeight;
+                    isGrounded = true;
+                }
             }
+            Debug.DrawRay(rayOrigin, rayDirection * rayDirection, Color.red); 
+          /*  if(pos.y <= groundHeight)
+            {
+              
+            }*/
+
            
         }
         distance += velocity.x * Time.fixedDeltaTime;
@@ -92,8 +107,22 @@ public class Player : MonoBehaviour
             {
                 velocity.x = maxXVelocity;
             }
+
+            Vector2 rayOrigin = new Vector2(pos.x - 0.7f, pos.y);
+            Vector2 rayDirection = Vector2.up;
+            float rayDistance = velocity.y * Time.fixedDeltaTime;
+            RaycastHit2D hit2d = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+            if (hit2d.collider == null)
+            {
+                isGrounded = false;
+            }
+            Debug.DrawRay(rayOrigin, rayDirection * rayDirection, Color.yellow);
+
+
         }
         transform.position = pos;
     }
 
 }
+
+
